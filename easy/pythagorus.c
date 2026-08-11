bc9ddb241a266d25df46b1abedef214c100786e0
pattr-hcu <690331@hcu.ac.th>
2026-08-11 13:24:57 +0700

    fix pythagorus

--- easy/pythagorus.c
+++ easy/pythagorus.c
@@ -5,12 +5,9 @@ int main() {
     double a, b, c;
 
     scanf("%lf %lf", &a, &b);
-    if (a < 0 || b < 0) {
-        printf("Input values must be non-negative.\n");
-        return 1;
-    }
-
+    
     c = sqrt(a * a + b * b);
+    
     printf("%.6f\n", c);
 
     return 0;
